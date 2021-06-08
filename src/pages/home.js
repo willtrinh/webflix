efa5b60a2f90834755cc6e0bfdca7e14fd778ad7
Will Trinh <williamltrinh@gmail.com>
2021-06-08 15:14:51 -0700

    Implemented feature component and added styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,25 @@ import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
 import { HeaderContainer } from '../containers/header';
+import { OptForm, Feature } from '../components';
+
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited movies, TV shows, and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+        </Feature>
+        <OptForm>
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Get Started</OptForm.Button>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
