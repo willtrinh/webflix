c66c6e5def6d4c46cf59b769f64011854f94a868
Will Trinh <williamltrinh@gmail.com>
2021-06-08 12:50:02 -0700

    Added skeleton for pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Browse, SignUp, SignIn } from './pages';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
