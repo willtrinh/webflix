f05e35131c46e8210e19ca95a9bcebb180e63bd4
Will Trinh <williamltrinh@gmail.com>
2021-06-08 18:03:42 -0700

    Created custom hooks for user auth

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, SignUp, SignIn } from './pages';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = null;
+  const { user } = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <Switch>
@@ -13,7 +15,6 @@ export default function App() {
           user={user}
           loggedInPath={ROUTES.BROWSE}
           path={ROUTES.SIGN_UP}
-          exact
         >
           <SignUp />
         </IsUserRedirect>
@@ -21,18 +22,16 @@ export default function App() {
           user={user}
           loggedInPath={ROUTES.BROWSE}
           path={ROUTES.SIGN_IN}
-          exact
         >
           <SignIn />
         </IsUserRedirect>
-        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
           <Browse />
         </ProtectedRoute>
         <IsUserRedirect
           user={user}
           loggedInPath={ROUTES.BROWSE}
           path={ROUTES.HOME}
-          exact
         >
           <Home />
         </IsUserRedirect>
