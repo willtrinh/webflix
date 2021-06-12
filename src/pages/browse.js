763eab5ac395ca4699bfb46a202afe924a5784e1
Will Trinh <williamltrinh@gmail.com>
2021-06-12 15:11:27 -0700

    Implemented filter util for films and series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useContent } from '../hooks';
-
+import selectionFilter from '../utils/selection-filter';
 export default function Browse() {
-  const { series } = useContent('series');
-  console.log(series);
+  const { films } = useContent('films');
+  console.log(films);
   return <>I'm Browse</>;
 }
