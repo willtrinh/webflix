71c36a03900c55781d7e09a0d0711d7991e00c43
Will Trinh <williamltrinh@gmail.com>
2021-06-12 15:51:46 -0700

    Added logo for browse and select profile component

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
 import { useContent } from '../hooks';
 import selectionFilter from '../utils/selection-filter';
 export default function Browse() {
   const { films } = useContent('films');
-  console.log(films);
-  return <>I'm Browse</>;
+  const { series } = useContent('series');
+  const slides = selectionFilter({ series, films });
+
+  return <BrowseContainer slides={slides} />;
 }
