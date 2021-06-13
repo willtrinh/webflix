9389737dd4b1bedbd20fc63c41bb574479f8fd27
Will Trinh <williamltrinh@gmail.com>
2021-06-13 16:22:09 -0700

    Implemented card feature to show detailed view of item

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -56,7 +56,8 @@ Card.Entities = function CardEntities({ children, ...restProps }) {
 };
 
 Card.Item = function CardItem({ item, children, ...restProps }) {
-  const [setShowFeature, setItemFeature] = useContext(FeatureContext);
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
   return (
     <Item
       onClick={() => {
@@ -72,4 +73,33 @@ Card.Item = function CardItem({ item, children, ...restProps }) {
 
 Card.Image = function CardImage({ ...restProps }) {
   return <Image {...restProps} />;
+};
+
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+  const { showFeature, itemFeature, setShowFeature } =
+    useContext(FeatureContext);
+
+  return showFeature ? (
+    <Feature
+      {...restProps}
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature.title}</FeatureTitle>
+        <FeatureText>{itemFeature.text}</FeatureText>
+        <FeatureClose onClick={() => setShowFeature(false)}>
+          <img src="/images/icons/close.png" alt="close" />
+        </FeatureClose>
+      </Content>
+      <Group margin="30px 0" flexDirection="row" alignItems="center">
+        <Maturity rating={itemFeature.maturity}>
+          {itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}
+        </Maturity>
+        <FeatureText fontWeight="bold">
+          {itemFeature.genre.charAt(0).toUpperCase() +
+            itemFeature.genre.slice(1)}
+        </FeatureText>
+      </Group>
+    </Feature>
+  ) : null;
 };
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -78,7 +78,33 @@ export function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
-      <Card.Group></Card.Group>
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              {/* <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player> */}
+              yo
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
