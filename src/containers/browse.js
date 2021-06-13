cbb346538ba9b8a68d3bdd09fc4747102e1b1b42
Will Trinh <williamltrinh@gmail.com>
2021-06-13 14:26:18 -0700

    Added Header feature into browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -17,7 +17,17 @@ export function BrowseContainer({ slides }) {
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
-      <Header src="joker1">hello</Header>
+      <Header src="joker1">
+        <Header.Feature>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
